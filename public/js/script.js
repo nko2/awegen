b1eb73fef86a6ab8adced81ceb3efc4a8542694b
Julio Monteiro <julio@monteiro.eti.br>
2011-08-27 18:31:24 -0300

    Improvements for index.html, Ace editor

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,12 @@
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
+  /* Ace editor */
+  var editor = ace.edit("sourcecode");
+  editor.setTheme("ace/theme/textmate");
+  var JavaScriptMode = require("ace/mode/javascript").Mode;
+  editor.getSession().setMode(new JavaScriptMode());
+
+  /* Socket.io */
   var socket = io.connect('http://localhost/');
   socket.emit('client-connect', 'ok');
 
@@ -31,7 +38,7 @@ $(function(){
     });
   });
   
-  /*
+  /* Backbone.js
   window.Graphic = Backbone.Model.extend({
     defaults: {
       source: '',
