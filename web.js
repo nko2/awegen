f094db058c53449d22393b8d85a3e62b5bf6d203
Felipe Gallois <felipe@gallois.com.br>
2011-08-27 14:00:43 -0300

    changing image output directory

--- web.js
+++ web.js
@@ -10,62 +10,60 @@ var app = express.createServer(express.logger());
 var server = io.listen(app);
 
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
-    //response.send('Hello Heroku!');
+  response.sendfile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
 
 server.sockets.on('connection', function (socket) {
 
-    // Send image list do client
-    socket.on('client-connect', function (data) {
-        console.log('Sending image list to client');
+  // Send image list to client
+  socket.on('client-connect', function (data) {
+    console.log('Sending image list to client');
 
-        fs.readdir(__dirname + '/images', function(err, files) {
-            if (err) throw err;
+    fs.readdir(__dirname + '/images', function(err, files) {
+      if (err) throw err;
 
-            console.log(sys.inspect(files));
-            socket.emit('image-list', JSON.stringify(files));
-        });
+      console.log(sys.inspect(files));
+      socket.emit('image-list', JSON.stringify(files));
     });
+  });
 
-    // Listen for sourcecode events from client
-    socket.on('sourcecode', function(data) {
-        console.log('Receiving sourcecode: \n' + data);
+  // Listen for sourcecode events from client
+  socket.on('sourcecode', function(data) {
+    console.log('Receiving sourcecode: \n' + data);
 
-        //var imageName = JSON.parse(data).sourcecode;
-        // tratar imagem
-        var json_message = JSON.parse(data).sourcecode;
-        var imageName = json_message.split(' ')[0]
-        var json_message = "images/" + json_message
-        var imageOutput = new Date().getTime() + imageName
-        var convert_params = json_message + " images/" + imageOutput
+    // tratar imagem
+    var json_message = JSON.parse(data).sourcecode;
+    var imageName = json_message.split(' ')[0]
+    var json_message = "images/" + json_message
+    var imageOutput = "output/" + new Date().getTime() + imageName
+    var convert_params = json_message + " images/" + imageOutput
 
-        child = exec("convert " + convert_params, function (error, stdout, stderr) {
-          console.log("stdout: " + stdout);
-          console.log("stderr: " + stderr);
+    child = exec("convert " + convert_params, function (error, stdout, stderr) {
+      console.log("stdout: " + stdout);
+      console.log("stderr: " + stderr);
 
-          var marvin = fs.readFileSync(__dirname + '/images/' + imageOutput);
+      var marvin = fs.readFileSync(__dirname + '/images/' + imageOutput);
 
-          if (error !== null) {
-            marvin = fs.readFileSync(__dirname + '/images/' + imageName);
-            console.log("stdout: " + stdout);
-          }
+      if (error !== null) {
+        marvin = fs.readFileSync(__dirname + '/images/' + imageName);
+        console.log("stdout: " + stdout);
+      }
 
-          var image = new Image;
-          image.src = marvin;
-          var canvas = new Canvas(image.width, image.height);
-          var ctx = canvas.getContext('2d');
-          ctx.drawImage(image, 0, 0, image.width, image.height);
+      var image = new Image;
+      image.src = marvin;
+      var canvas = new Canvas(image.width, image.height);
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(image, 0, 0, image.width, image.height);
 
-          var data = '<img src="' + canvas.toDataURL() + '"/>';
-          socket.emit('image', data);
-        });
+      var data = '<img src="' + canvas.toDataURL() + '"/>';
+      socket.emit('image', data);
     });
+  });
 });
 
 
